222e62dd417c4775b32f7263a6fd37d435a8256c
javiBarrientos <javibarrientosfernandez99@gmail.com>
2020-02-26 15:38:47 +0100

    test: Añadir test ufospark
    queda refactorizar logica, test mal hechos

--- src/test/java/edu/elsmancs/ricksybusiness/domain/UfosParkTest.java
+++ src/test/java/edu/elsmancs/ricksybusiness/domain/UfosParkTest.java
@@ -1,6 +1,8 @@
 package edu.elsmancs.ricksybusiness.domain;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -12,5 +14,24 @@ public class UfosParkTest {
     public void testAñadirUfo() {
         UfosPark ufosPark = new UfosPark();
         CreditCard creditCard = new CreditCard("Abradolph Lincler", "4916119711304546");
+
+        ufosPark.add("trex");
+        ufosPark.add("dox");
+        System.out.println(ufosPark.getFlota());
+
+        assertEquals("trex", ufosPark.getUfoOf(creditCard.toString()));
+    }
+
+    @Test
+    public void comprobarUfo() {
+        UfosPark ufosPark = new UfosPark();
+        CreditCard creditCard = new CreditCard("Abradolph Lincler", "4916119711304546");
+
+        assertEquals("dox", ufosPark.getUfoOf(creditCard.toString()));
+    }
+
+    @Test
+    public void testAñadirValor() {
+
     }
 }
