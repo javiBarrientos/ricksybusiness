ebdf5a52bcbeac915960bf49bdf704cf6f6eca0b
Javi <javibarrientosfernandez99@gmail.com>
2020-05-17 16:59:26 +0200

    doc, refactor: Refactorizar logica, añadir comentarios

--- src/main/java/edu/elsmancs/ricksybusiness/domain/UfosPark.java
+++ src/main/java/edu/elsmancs/ricksybusiness/domain/UfosPark.java
@@ -11,31 +11,52 @@ public class UfosPark implements GuestDispatcher{
 
     }
 
+    /**
+     * getter que te devuelve la flota.
+     * @return
+     */
+
     Map getFlota() {
         return this.flota = flota;
     }
+
+    /**
+     * setter, este metodo necesita que le pases
+     * @param ovni
+     */
 
     void add(String ovni) {
         flota.put(ovni, null);
     }
 
+    /**
+     * Metodo de la interfaz de GuestDispatcher,
+     * este metodo necesita de una tarjeta de credito,
+     * recorre el hashmap de flota buscando las entradas,
+     * comprueba que si el ovni es null y tiene mas dinero de lo que cuesta que lo meta,
+     * el break es para cortar el proceso y no meta keys duplicadas.
+     *
+     * Este metodo queda refactorizarlo.
+     * @param creditCard
+     */
+
     @Override
-    public void dispatch(CreditCard card) {
-
-        Map.Entry<String, String> ufo = null;
-
-        if (!flota.containsValue(card.number())) {
-            for (Map.Entry<String, String> entry : this.flota.entrySet()) {
-                if (entry.getValue() == null) {
-                    ufo = entry;
-                    break;
-                }
+    public void dispatch(CreditCard creditCard) {
+        for (Map.Entry<String, String> ovni : flota.entrySet()) {
+            if (ovni.getValue() == null && creditCard.credit() >= fee) {
+                creditCard.pay(fee);
+                flota.put(ovni.getKey(), creditCard.number());
+                break;
             }
         }
-        if (ufo != null  && card.pay(fee)) {
-            this.flota.put(ufo.getKey(), card.number());
-        }
     }
+
+    /**
+     * Este metodo requiere de un owner,
+     * recorre el mapa y busca un ovni con owner y cuando lo encuentra devuelve el nombre del ovni.
+     * @param owner
+     * @return
+     */
 
     String getUfoOf(String owner) {
         String ovniName = "";
@@ -47,6 +68,11 @@ public class UfosPark implements GuestDispatcher{
         }
         return ovniName;
     }
+
+    /**
+     * Metodo toString
+     * @return
+     */
 
     @Override
     public String toString() {
