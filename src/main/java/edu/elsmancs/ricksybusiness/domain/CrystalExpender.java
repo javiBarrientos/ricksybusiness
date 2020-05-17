97420dcd9e692e21116738ef47a43e0f61ec1229
Javi <javibarrientosfernandez99@gmail.com>
2020-05-17 16:33:22 +0200

    doc: Añadir comentarios

--- src/main/java/edu/elsmancs/ricksybusiness/domain/CrystalExpender.java
+++ src/main/java/edu/elsmancs/ricksybusiness/domain/CrystalExpender.java
@@ -4,15 +4,38 @@ public class CrystalExpender implements GuestDispatcher{
     private int stock = 0;
     private double itemCost = 0;
 
+    /**
+     * Constructor de la clase CrystalExpender,
+     * cada vez que llames a la clase te tendras que pasar los parametros de stock e itemcost.
+     * @param stock
+     * @param itemCost
+     */
+
     CrystalExpender(int stock, double itemCost) {
         this.stock = stock;
         this.itemCost = itemCost;
     }
 
-    /*
-    * Comprobar stock
-    * Comprobar dinero
+    /**
+     * Este getter cuando lo invocas te devuelve el stock.
+     * @return
      */
+
+    int stock() {
+        return this.stock = stock;
+    }
+
+    /**
+     * Esta funcion necesita una tarjeta de credito,
+     * primer if comprueba que hay stock,
+     * segundo if comprueba que tiene pasta y al final resta 1 al stock,
+     * esta funcion no devuelve nada.
+     *
+     * Al ser una clase que implementa una interfaz, si queremos usar los metodos de le interfaz los tendremos que
+     * sobreescribir.
+     * @param creditCard
+     */
+
     @Override
     public void dispatch(CreditCard creditCard) {
         if (stock() > 0) {
@@ -23,9 +46,10 @@ public class CrystalExpender implements GuestDispatcher{
         }
     }
 
-    int stock() {
-        return this.stock = stock;
-    }
+    /**
+     * Metodo toString donde mostramos el stock con el coste de cada item.
+     * @return
+     */
 
     @Override
     public String toString() {
